44967260cc9920b91f2c05d4f3b5dd4d8d57e694
peds <hugh1127@gmail.com>
2025-02-21 16:41:01 +0900

    feat: implement auth_users.js (Task 7~9)

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,42 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{ username: "user1", password: "1234" }];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  return username !== "";
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  return !!users.find(
+    (user) => user.username === username && user.password === password
+  );
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!authenticatedUser(username, password)) {
+    return res.sendStatus(401);
+  }
+
+  const token = jwt.sign({ username }, "secret", { expiresIn: 60 * 60 });
+  return res.status(300).json({ token });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const reviews = books[req.params.isbn].reviews;
+  reviews[req.session.username] = req.body.review;
+  return res.status(200).json(reviews);
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const reviews = books[req.params.isbn].reviews;
+  delete reviews[req.session.username];
+  return res.status(200).json(reviews);
 });
 
 module.exports.authenticated = regd_users;
